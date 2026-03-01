d76a34aa1fc11afc29fcb3f627249dce63ef58ef
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-03-01 12:52:19 +0530

    Problem 1 solved

--- Linear Search/problem_1.js
+++ Linear Search/problem_1.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+// Question 1: Find Element Index
+// Return the index of the target number.
+
+function linearSearch(arr, target){
+    for(let i = 0; i< arr.length; i++ ){
+        if(arr[i] === target){
+            return i;
+        }else{
+            console.log("Not found")
+        }
+    }
+}
+
+console.log(linearSearch([1,2,3,4,5,9,7,8,5,4],8));
